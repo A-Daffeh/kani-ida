54c2a0e12bf9627e6f0cdc956fc5b3c68ac31541
Adama <adama.daffeh07@gmail.com>
2024-05-04 09:58:48 -0700

    Project Initialization

---
+++ src/main/java/com/mcg/kaniida/KaniIdaApplication.java
@@ -0,0 +1,13 @@
+package com.mcg.kaniida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaniIdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaniIdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcg/kaniida/KaniIdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcg.kaniida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaniIdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
